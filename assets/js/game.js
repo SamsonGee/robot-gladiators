f8766a320385d3a8d2e43cc88477ccc549c835a7
SamsonGee <88246655+SamsonGee@users.noreply.github.com>
2021-08-23 07:19:19 -0500

    array fun

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -6,19 +12,34 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+console.log(enemyNames);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
+
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+  }
+
+// fight function 
 var fight = function() {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
+    // ask player if they'd like to fight or run
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
     // if player choses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
     
-        // remove enemy's health by subtracting the amount set in the playerAttack variable
+    // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     
     console.log(
@@ -52,7 +73,8 @@ var fight = function() {
     
     // if player choses to skip
   
-    } else if (promptFight === "skip" || promptFight === "SKIP") {
+    } 
+    else if (promptFight === "skip" || promptFight === "SKIP") {
     // confirm player wants to skip
         var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
@@ -65,6 +87,14 @@ var fight = function() {
     }
     // if no (false), ask question again by running fight() again
     else {
-        fight();}
+        fight();
     }
-}
+   
+    // if player did not chose 1 or 2 in prompt
+    } 
+    else {
+        window.alert("You need to pick a valid option. Try again!");
+    }   
+};
+
+// fight ();
